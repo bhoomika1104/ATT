255e793a2b2956c871a617d4b5ffa587f2360070
Bhoomika R Gowda <bhoomikargowda2004@gmail.com>
2025-06-03 16:10:21 +0530

    Create StringsInJava.java

---
+++ StringsInJava.java
@@ -0,0 +1,7 @@
+public class StringsInJava {
+    public static void main(String[] args) {
+        char[] letters= new char[]{'a', 'n', 'n', 'a'};
+        String name= new String(letters);
+        System.out.println(name);
+    }
+}
